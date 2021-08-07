8e81676b266022adbe4c4a698c1704a48ed5cf36
Robb Knight <desk@rknight.me>
2021-08-07 16:01:32 +0100

    Remove www. too. Only show URL, no other data

--- src/pocket.py
+++ src/pocket.py
@@ -206,9 +206,9 @@ def get_subtitle(item_count, time_updated, given_url, tags=None):
     time_updated = datetime.datetime.fromtimestamp(
         int(time_updated)).strftime('%Y-%m-%d %H:%M')
     short_url = given_url.replace(
-        'http://', '').replace('https://', '')
+        'http://', '').replace('https://', '').replace('www.', '')
 
-    subtitle_elements = ['#%s' % item_count, time_updated, short_url]
+    subtitle_elements = [short_url]
 
     if tags:
         tags = ['#%s' % x for x in tags.keys()]
